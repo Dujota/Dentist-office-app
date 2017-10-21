b5c37bb3632a85c7ef9b33b09434627cdc11bade
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-21 12:18:57 -0400

    create new appointment view and form complerted added the drop down menu for the doctors and the date picker for the appoitnment_at

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :appointments
   # has_many :dentists, through: :appointments
 
+  validates :name, :email, :phone, :password, presence: true
+  validates :email, uniqueness: true
 end
