690b0ea0316cab6d74b1b1f9c7a508c0d8bbc754
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-21 17:51:48 -0400

    added the send text api functionality

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,13 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :dentist
 
+  def send_confirmation_sms
+    client = Twilio::REST::Client.new(ENV['TWILIO_ID'], ENV['TWILIO_TOKEN'])
+
+    client.api.account.messages.create(
+    from: ENV['PHONE_NUMBER'],
+    to: "+1#{self.user.phone}",
+    body: "You have an Appointment on #{self.appointment_at} with #{self.dentist.name}"
+    )
+  end
 end
