b703a98b73bb736af0a3965a7df8dc4867c17a2a
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-21 17:20:57 -0400

    completed the cancel button on our admin app controller

--- app/controllers/admin/appointments_controller.rb
+++ app/controllers/admin/appointments_controller.rb
@@ -1,5 +1,5 @@
 class Admin::AppointmentsController < Admin::BaseController
-  before_action :load_appointment, only: [:confirm]
+  before_action :load_appointment, only: [:confirm, :cancel]
 
   def index
     @appointments = Appointment.order(created_at: :desc) # we dont care about displaying as the admin is supposed to see all appointments
@@ -9,6 +9,13 @@ class Admin::AppointmentsController < Admin::BaseController
     @appointment.status = 'confirmed'
     @appointment.save!
     flash[:notice] = "Appointment has been confirmed"
+    redirect_to admin_appointments_url
+  end
+
+  def cancel
+    @appointment.status = 'canceled'
+    @appointment.save!
+    flash[:notice] = "Appointment has been thrown in the garbage!"
     redirect_to admin_appointments_url
   end
 
