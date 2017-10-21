0561e123cf4a6e97003182ed77a268d96ee11a35
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-21 11:43:00 -0400

    installed gem, added route for appointments, defined the users create method, with the if statement for save that prompts a message if it saves or doenst and rememmbers the user logged in via session[user_id]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    #code
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Hold on to your wallet, you 'bout to get shcammzed"
+      session[:user_id] = @user.id  # this will remember and keep the user logged in by saving the info in their session cookie
+      redirect_to appointments_url
+    else
+      flash[:alert] = "You dun goofed son! Fix dat chit"
+      render :new
+    end
+
   end
 
+  private
+  #this mehtod passes in the params from the form to the create method
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :phone)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-
+  has_secure_password
   has_many :appointments
   # has_many :dentists, through: :appointments
-  
+
 end
