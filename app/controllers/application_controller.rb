3db70476d1d218f490d9ea5d84d43cf313d6072a
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-21 12:35:01 -0400

    setup helper methods : current_user, logged_in? in the application controller to find the user and check if they are logged in and then saved the form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    User.find(session[:user_id]) if logged_in?
+  end
+
+  helper_method :current_user # helper_method lets you use this shorthand in the views as well
+
+  def logged_in?
+    session[:user_id].present?
+  end
+
+  helper_method :logged_in?
 end
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -7,5 +7,22 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.new
   end
 
+  def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.user_id = current_user.id
+
+    if @appointment.save
+      flash[:notice] = "Your appointment has been saved"
+      redirect_to appointments_url
+    else
+      render :new
+    end
+  end
+
+  private
+  #this mehtod passes in the params from the form to the create method
+  def appointment_params
+    params.require(:appointment).permit(:dentist_id, :appointment_at)
+  end
 
 end
